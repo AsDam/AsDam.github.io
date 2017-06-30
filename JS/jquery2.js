44780b21bced411283a5c6a8bbc354b1edded6a0
AsDam <asdam2017emp@gmail.com>
2017-06-30 12:04:13 -0500

    Intial commit

--- JS/jquery2.js
+++ JS/jquery2.js
@@ -1,5 +1,7 @@
 $(document).ready(function(){
- 
+    var imgItems = $('.slider li').length; // Numero de Slides
+	var imgPos = 1;
+
 	$('.ir-arriba').click(function(){
 		$('body, html').animate({
 			scrollTop: '0px'
@@ -13,5 +15,48 @@ $(document).ready(function(){
 			$('.ir-arriba').slideUp(300);
 		}
 	});
- 
-});
+	
+	// Agregando paginacion --
+	for(i = 1; i <= imgItems; i++){
+		$('.pagination').append('<li><span class="fa fa-circle"></span></li>');
+	} 
+	//------------------------
+
+	$('.slider li').hide(); // Ocultanos todos los slides
+	$('.slider li:first').show(); // Mostramos el primer slide
+	$('.pagination li:first').css({'color': '#CD6E2E'}); // Damos estilos al primer item de la paginacion
+
+	// Ejecutamos todas las funciones
+	$('.right span').click(nextSlider);
+	$('.left span').click(prevSlider);
+
+
+	setInterval(function(){
+		nextSlider();
+	}, 7000);
+
+	// FUNCIONES =========================================================
+
+	
+
+	function nextSlider(){
+		if( imgPos >= imgItems){imgPos = 1;} 
+		else {imgPos++;}
+		$('.slider li').hide(); // Ocultamos todos los slides
+		$('.slider li:nth-child('+ imgPos +')').fadeIn(); // Mostramos el Slide seleccionado
+        clearInterval(interval);
+         interval = setInterval(nextSlider,7000);
+	}
+
+	function prevSlider(){
+		if( imgPos <= 1){imgPos = imgItems;} 
+		else {imgPos--;}
+		$('.slider li').hide(); // Ocultamos todos los slides
+		$('.slider li:nth-child('+ imgPos +')').fadeIn(); // Mostramos el Slide seleccionado
+		clearInterval(interval);
+         interval = setInterval(nextSlider,7000);
+
+	}
+
+   
+})
